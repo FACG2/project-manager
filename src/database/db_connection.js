492e7f534144d6be6017f3d01721e09f39aed2e4
samer elaila <samer-elaila@live.com>
2017-08-16 10:51:22 +0300

    implement back-end requests Relates #21

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -6,6 +6,6 @@ if(!process.env.DATABASE_URL || !process.env.DB_URL){
 }
 
 
-const pool = new Pool({ connectionString: process.env.DB_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
